4be5c8bb12109e334aedf3ee0d3e938b618a1d1d
Aniruddha <anshuljaiswal2211@gmail.com>
2020-09-30 14:21:00 +0530

    commit calculator api

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require("body-parser");
 const port = 3000
+
 app.use(express.urlencoded());
 
 // Parse JSON bodies (as sent by API clients)
@@ -15,6 +16,115 @@ app.use(bodyParser.json())
 
 // here
 
+app.get("/",(req,res)=>{
+    res.send("Hello World!");
+})
+
+app.post("/add",(req,res)=>{
+    let num1 = req.body.num1;
+    let num2 = req.body.num2;
+    let result = num1 + num2;
+    let max = 1000000;
+
+    if(typeof num1 == "string" || typeof num2 == "string"){
+        res.json({
+            status: "error",
+            message: "Invalid data types"
+        })
+    }
+    else if(num1>=max || num2>=max || result>=max){
+        res.json({
+            status: "error",
+            message: "Overflow"
+        })
+    }
+    else{
+        res.json({
+            status: "success",
+            message: "the sum of given two numbers",
+            sum: result
+        })
+    }
+})
+
+app.post("/sub",(req,res)=>{
+    let num1 = req.body.num1;
+    let num2 = req.body.num2;
+    let result = num1 - num2;
+
+    if(typeof num1 == "string" || typeof num2 == "string"){
+        res.json({
+            status: "error",
+            message: "Invalid data types"
+        })
+    }
+    else if(result<(-1000000)){
+        res.json({
+            status: "error",
+            message: "Underflow"
+        })
+    }
+    else{
+        res.json({
+            status: "success",
+            message: "the difference of given two numbers",
+            difference: result
+        })
+    }
+})
+
+app.post("/multiply",(req,res)=>{
+    let num1 = req.body.num1;
+    let num2 = req.body.num2;
+    let result = num1 * num2;
+    let max = 1000000;
+
+    if(typeof num1 == "string" || typeof num2 == "string"){
+        res.json({
+            status: "error",
+            message: "Invalid data types"
+        })
+    }
+    else if(num1>max || num2>max || result>max){
+        res.json({
+            status: "error",
+            message: "Overflow"
+        })
+    }
+    else{
+        res.json({
+            status: "success",
+            message: "The product of given numbers",
+            result: result
+        })
+    }
+})
+
+app.post("/divide",(req,res)=>{
+    let num1 = req.body.num1;
+    let num2 = req.body.num2;
+
+    if(typeof num1 == "string" || typeof num2 == "string"){
+        res.json({
+            status: "error",
+            message: "Invalid data types"
+        })
+    }
+    else if(num2==0){
+        res.json({
+            status: "error",
+            message: "Cannot divide by zero"
+        })
+    }
+    else{
+        let result = num1/num2;
+        res.json({
+            status: "success",
+            message: "The division of given numbers",
+            result: result
+        })
+    }
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
